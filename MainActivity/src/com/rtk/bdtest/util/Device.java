c629ede410d6aa6b6d37208fe879901d32fde1cc
zhudw3 <zdwxxx@qq.com>
2014-10-14 20:57:55 +0800

    zigbee demo git project init,we can send information using zigbee on android device.

---
+++ MainActivity/src/com/rtk/bdtest/util/Device.java
@@ -0,0 +1,9 @@
+package com.rtk.bdtest.util;
+
+public class Device {
+	public String deviceAddress;
+	public String deviceType;
+	public String deviceID;
+	public String parentAddress;
+	public int count;
+}
